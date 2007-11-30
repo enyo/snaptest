c64e9491d9d7603a7011285d936068639d09f053
rjheuser <rjheuser@85759191-e132-0410-920c-21c98535aa7c>
2007-11-30 06:16:14 +0000

    bootstrap file added for launching tests
    git-svn-id: https://snaptest.googlecode.com/svn/trunk@28 85759191-e132-0410-920c-21c98535aa7c

---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Snaptest Bootstrap File
+ **/
+include_once 'snap.php';
+
+if (!isset($argv) && !is_array($argv)) {
+    echo "\nUSAGE: php test.php <path>\n";
+    exit;
+}
+
+array_shift($argv);
+
+foreach ($argv as $arg) {
+    if (strpos($arg, '--') === FALSE) {
+        // testable file
+        if (!file_exists($arg)) {
+            echo "\nFile ".$arg." does not exist.\n";
+            exit;
+        }
+        include $arg;
+    }
+}
+
+
