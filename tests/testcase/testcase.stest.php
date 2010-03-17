0695f96f6396d5a67d7e97cabfdf7427a456711c
Jakob Heuser <jakob@felocity.org>
2010-03-17 17:14:04 -0500

    added fatal error support to serialized php output in the test aggregator

--- tests/testcase/testcase.stest.php
+++ tests/testcase/testcase.stest.php
@@ -329,3 +329,36 @@ class Snap_UnitTestCase_Calling_Skip_In_Setup extends Snap_UnitTestCase {
         return $this->assertCallCount($this->reporter, 'recordTestSkip', 1);
     }
 }
+
+// handles exceptions being thrown
+class Snap_UnitTestCase_WillThrow_Test extends Snap_UnitTestCase {
+    public function setUp() {}
+    public function tearDown() {}
+    public function testWillThrowPasses() {
+        $this->willThrow('Exception');
+        throw new Exception('foo');
+    }
+}
+
+// disabled. Fatal errors must always fall through as failures
+// issue #4
+// handles fatal errors
+class Snap_UnitTestCase_WillError_Test extends Snap_UnitTestCase {
+    public function setUp() {}
+    public function tearDown() {}
+    public function testWillErrorPasses() {
+        $this->willError();
+        ljifsdkjndjkdjkdsjk(); // call to function with no arguments
+    }
+}
+
+// handles warnings
+class Snap_UnitTestCase_WillWarn_Test extends Snap_UnitTestCase {
+    public function setUp() {}
+    public function tearDown() {}
+    public function testWillWarnPasses() {
+        $this->willWarn();
+        $file = md5(time().file_get_contents(__FILE__));
+        include $file;
+    }
+}
